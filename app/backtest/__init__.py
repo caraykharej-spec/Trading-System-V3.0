27471624980c827d2a62420860fc25a203ba0def
caraykharej-spec <caraykharej@gmail.com>
2026-09-10 10:18:18 +0330

    feat: export backtest cost model

--- app/backtest/__init__.py
+++ app/backtest/__init__.py
@@ -1,6 +1,14 @@
 """Historical backtesting and simulation."""
 
+from .costs import BacktestCostModel, CostBreakdown
 from .engine import BacktestEngine
 from .models import BacktestConfig, BacktestResult, TradeRecord
 
-__all__ = ["BacktestEngine", "BacktestConfig", "BacktestResult", "TradeRecord"]
+__all__ = [
+    "BacktestEngine",
+    "BacktestConfig",
+    "BacktestResult",
+    "TradeRecord",
+    "BacktestCostModel",
+    "CostBreakdown",
+]
