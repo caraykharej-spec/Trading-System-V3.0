1d573cde58b9983d889126fdc30df407c1e567a0
caraykharej-spec <caraykharej@gmail.com>
2026-09-10 22:42:08 +0330

    Add report templates foundation for Phase 31.4

---
+++ app/reporting/report_templates.py
@@ -0,0 +1,15 @@
+"""Report templates foundation."""
+
+DAILY_TRADING_REPORT = "daily_trading_report"
+PERFORMANCE_REPORT = "performance_report"
+RISK_REPORT = "risk_report"
+STRATEGY_REPORT = "strategy_report"
+PORTFOLIO_SUMMARY = "portfolio_summary"
+
+REPORT_TEMPLATES = {
+    DAILY_TRADING_REPORT: {},
+    PERFORMANCE_REPORT: {},
+    RISK_REPORT: {},
+    STRATEGY_REPORT: {},
+    PORTFOLIO_SUMMARY: {},
+}
